7b6baf2001385dbc7677246712ad5ae15bcac532
Alessandro Cosentino <cosenal@gmail.com>
2025-01-01 16:52:16 +0100

    Fix repo date script

--- GitHub_Repo_Dates/GitHub_Repo_Dates.user.js
+++ GitHub_Repo_Dates/GitHub_Repo_Dates.user.js
@@ -1,24 +1,22 @@
 // ==UserScript==
-// @name        GitHub Repo Dates
-// @license     MIT
-// @namespace   https://github.com/cosenal/Userscripts
-// @description Show the date on which a GitHub repo was created
-// @include     https://github.com/*
-// @version     0.0.1
-// @run-at      document-end
-// @grant       GM_xmlhttpRequest
+// @name         GitHub Repo Dates
+// @license      MIT
+// @namespace    https://github.com/cosenal/user-scripts
+// @version      0.1.0
+// @description  Display creation date of GitHub repositories
+// @author       cosenal
+// @match        https://github.com/*/*
+// @grant        GM_xmlhttpRequest
+// @connect      api.github.com
 // ==/UserScript==
 
-// ==OpenUserJS==
-// @author cosenal
-// ==/OpenUserJS==
 
 (function() {
-
-    var extraInfo = document.createElement("div");
-    extraInfo.style =
-        'padding-top: 10px;' +
-        'font-style: italic;';
+    // Ensure we are on the main repository page
+    const pathParts = document.location.pathname.split('/');
+    if (pathParts.length > 3 && pathParts[3] !== "") {
+        return;
+    }
 
     function fetchData(repo_full_name) {
         GM_xmlhttpRequest({
@@ -29,20 +27,26 @@
     }
 
     function fillData(response) {
-        parsedRes = JSON.parse(response.responseText);
+        var parsedRes = JSON.parse(response.responseText);
         var date = new Date(parsedRes.created_at);
-        var content = "Created on " + date.toDateString()
+        var options = { year: 'numeric', month: 'long' };
+        var formattedDate = date.toLocaleDateString('en', options);
+
+        var content = "Since " + formattedDate;
         var extraInfoContent = document.createTextNode(content);
+
+        var extraInfo = document.createElement("div");
         extraInfo.appendChild(extraInfoContent);
+        var headers = document.querySelectorAll('.BorderGrid-cell h3');
+        headers.forEach(function(header) {
+            if (header.textContent.trim() === "Resources") {
+                header.parentNode.insertBefore(extraInfo, header.nextSibling);
+            }
+        });
     }
 
-    var repoMetaDiv = document.getElementsByClassName('repository-meta-content'); 
-    if (repoMetaDiv.length > 0) {
-        var parser = document.createElement('a');
-        parser.href = document.querySelector("link[rel=canonical]").getAttribute("href");
-        var str = parser.pathname
-        fetchData(str.slice(1, str.length));
-        repoMetaDiv[0].appendChild(extraInfo);
-    }
+    var canonicalLink = document.querySelector('link[rel=canonical]').getAttribute("href");
+    var repoPath = new URL(canonicalLink).pathname.slice(1);
 
+    fetchData(repoPath);
 })();
